f010bb3dec8b85a2e39630f2e4173f6bc452cdb1
wrochcode <wrochcode@gmail.com>
2021-09-26 11:13:45 +0700

    rename of controller

--- application/controllers/Service.php
+++ application/controllers/Service.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Service extends CI_Controller
+class service extends CI_Controller
 {
     public function index()
     {
