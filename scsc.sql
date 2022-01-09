b5582318cf355c9dde499310e3397ba4efabe195
wrochcode <wrochcode@gmail.com>
2022-01-09 19:42:29 +0700

    update

--- scsc.sql
+++ scsc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 06, 2022 at 03:58 PM
+-- Generation Time: Jan 08, 2022 at 04:55 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
